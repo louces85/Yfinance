154eaf89033d504caecd702467b412d752ee498b
Fabiano Louzada <fabiano.cesario.k@gmail.com>
2021-11-25 19:21:46 -0300

    add enum rules

---
+++ src/enums/rules.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Rules(Enum):
+    D_Y =  5
+    P_L =  15
+    P_VP = 1.5
+    DL_PL= 1
+    DL_EBITDA = 3
+    P_A = 1
+    L_Q = 1
+    M_EBIT = 10
+    M_L = 10
+    ROE = 10
+    ROIC = 10
+    CAGR_R = 5
+    CAGR_L = 5
+    BASIN = 0.06
+    
