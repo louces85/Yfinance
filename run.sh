bfdee7dd3b4d4e3efe07cff4d23dfadbaaa0b851
Fabiano Louzada <fabiano.cesario.k@gmail.com>
2021-12-02 22:38:44 -0300

    Add patch and python version

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd ~/Documents/Yfinance
+path=$(dirname "$0")
+if [[ "$path" == "." ]]; then
+    path=$(pwd)
+fi
+
+cd $path
 
 if [ ! -s files/history_6mo_results_net_income ]
 	then
@@ -8,8 +13,15 @@ if [ ! -s files/history_6mo_results_net_income ]
 		exit 0
 	fi
 
-cat css/stylesheet.css > index.html 
-python3 src/main.py >> index.html
+python_version=`python -c 'import sys; print(".".join(map(str, sys.version_info[:1])))'`
+if [ $python_version -eq 2 ]
+	then
+		cat css/stylesheet.css > index.html 
+		python3 src/main.py >> index.html
+else
+		cat css/stylesheet.css > index.html 
+		python src/main.py >> index.html
+fi
 
 for k in $(cat index.html | grep "padding-left: 1em; padding-right: 1em; text-align: left; vertical-align: top" | awk -F ">" '{print $2}' | awk -F "</" '{print $1}')
 do
