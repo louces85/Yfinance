12465d8ea93ab021461235577b95fcd92dba8c13
Fabiano Louzada <fabiano.cesario.k@gmail.com>
2021-12-16 20:15:41 -0300

    suport to Linux and CYGWIN

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+unameOUT=`uname -s`
+docker_exec=""
+case "${unameOUT}" in
+    Linux*)     docker_exec="docker exec -it yfinance-postgres psql -U postgres yfinance -c";;
+    CYGWIN*)    docker_exec="winpty docker exec -it yfinance-postgres psql -U postgres yfinance -c";;
+    *)          exit 0
+esac
+
 function create_data_base(){
-    docker exec -it yfinance-postgres psql -U postgres yfinance -c "CREATE TABLE if not exists history ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_min float8 null, price_max float8 null, net_income boolean null, date_update date null );"
-    docker exec -it yfinance-postgres psql -U postgres yfinance -c "CREATE TABLE if not exists stock ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE );"
-    docker exec -it yfinance-postgres psql -U postgres yfinance -c "CREATE TABLE if not exists valuation ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_target float8 null, payout float8 null );"
-    docker exec -it yfinance-postgres psql -U postgres yfinance -c "CREATE TABLE if not exists price ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_now float8 null );"
+    $docker_exec "CREATE TABLE if not exists history ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_min float8 null, price_max float8 null, net_income boolean null, date_update date null );"
+    $docker_exec "CREATE TABLE if not exists stock ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE );"
+    $docker_exec "CREATE TABLE if not exists valuation ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_target float8 null, payout float8 null );"
+    $docker_exec "CREATE TABLE if not exists price ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_now float8 null );"
 }
 create_data_base
--- scripts/insert_default_values_database.sh
+++ scripts/insert_default_values_database.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
+sed -i 's/\r//g' ../files/stocks_file
+
 stocks=`cat ../files/stocks_file`
+
+unameOUT=`uname -s`
+docker_exec=""
+case "${unameOUT}" in
+    Linux*)     docker_exec="docker exec -it yfinance-postgres psql -U postgres yfinance -c";;
+    CYGWIN*)    docker_exec="winpty docker exec -it yfinance-postgres psql -U postgres yfinance -c";;
+    *)          exit 0
+esac
 
 function insert_values_stock(){
     for k in $stocks
     do
-        docker exec -it yfinance-postgres psql -U postgres yfinance -c "insert into stock (ticker) values ('$k');"
+        $docker_exec "insert into stock (ticker) values ('$k');"
     done
 }
 
 function insert_values_price(){
     for k in $stocks
     do
-        docker exec -it yfinance-postgres psql -U postgres yfinance -c "insert into price (ticker, price_now) values ('$k', 0);"
+        $docker_exec "insert into price (ticker, price_now) values ('$k', 0);"
     done
 }
 
 function insert_values_history(){
     for k in $stocks
     do
-        docker exec -it yfinance-postgres psql -U postgres yfinance -c "insert into history (ticker, price_min, price_max, net_income, date_update) values ('$k', 0, 0, false, '1969-12-31');"
+        $docker_exec "insert into history (ticker, price_min, price_max, net_income, date_update) values ('$k', 0, 0, false, '1969-12-31');"
     done
 }
 
 function insert_values_valuation(){
     for k in $stocks
     do
-        docker exec -it yfinance-postgres psql -U postgres yfinance -c "insert into valuation (ticker, price_target, payout) values ('$k', 0, 0);"
+        $docker_exec "insert into valuation (ticker, price_target, payout) values ('$k', 0, 0);"
     done
 }
 
