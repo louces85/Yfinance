50bc4e451e95dc455847d6fab1d6d9441c93ff71
Fabiano Louzada <fabiano.cesario.k@gmail.com>
2021-12-16 19:44:45 -0300

    Fixing a bug in *database.sh

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -6,4 +6,4 @@ function create_data_base(){
     docker exec -it yfinance-postgres psql -U postgres yfinance -c "CREATE TABLE if not exists valuation ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_target float8 null, payout float8 null );"
     docker exec -it yfinance-postgres psql -U postgres yfinance -c "CREATE TABLE if not exists price ( id SERIAL PRIMARY KEY, ticker VARCHAR(7) NOT null UNIQUE, price_now float8 null );"
 }
-create_data_base()
+create_data_base
