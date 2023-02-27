e6a0986f05bb42cdfa36b2a70118bd625c5b9a61
Fabiano Louzada <fabiano.cesario.k@gmail.com>
2023-02-26 23:32:38 -0300

    fix bugs

--- scripts/insert_default_values_database.sh
+++ scripts/insert_default_values_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i 's/\r//g' ../files/stocks_file
+sed -i 's/\r//g' ../files/stocks_file_2023
 
-stocks=`cat ../files/stocks_file`
+stocks=`cat ../files/stocks_file_2023`
 
 unameOUT=`uname -s`
 docker_exec=""
